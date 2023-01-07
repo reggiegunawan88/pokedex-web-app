bd3a7b2895e321bfb9f006dec356dbaf15d67640
reggiegunawan88 <reggiegunawan88@gmail.com>
2023-01-07 16:16:47 +0700

    fix(pokedex): close dialog after catch pokemon

--- src/hooks/components/ModalDialog/useCatchPokemonDialog.ts
+++ src/hooks/components/ModalDialog/useCatchPokemonDialog.ts
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 
+import useDialogStore from '@/store/Dialog';
 import useMyPokemonCollection from '@/store/MyPokemonCollection';
+import useSnackbarStore from '@/store/Snackbar';
 
 interface IProps {
   pokemon: {
@@ -16,6 +18,16 @@ const useCatchPokemonDialog = ({ pokemon }: IProps) => {
   const { storePokemon } = useMyPokemonCollection(state => ({
     storePokemon: state.storePokemon,
   }));
+  const { closeDialog } = useDialogStore(state => ({
+    closeDialog: state.closeDialog,
+  }));
+
+  const { openSnackbar, setSnackbarDescription, setSnackbarType } =
+    useSnackbarStore(state => ({
+      openSnackbar: state.openSnackbar,
+      setSnackbarDescription: state.setSnackbarDescription,
+      setSnackbarType: state.setSnackbarType,
+    }));
   const [nickname, setNickname] = useState<string>('');
 
   const setPokemonNickname = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -28,6 +40,10 @@ const useCatchPokemonDialog = ({ pokemon }: IProps) => {
       nickname,
     };
     storePokemon(pokemonData);
+    closeDialog();
+    openSnackbar();
+    setSnackbarDescription('Successfully caught pokemon!');
+    setSnackbarType('SUCCESS');
   };
 
   return { setPokemonNickname, savePokemon };
